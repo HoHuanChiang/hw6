e851990348646e30ac0510e46fa2d69844be5949
Ho-Huan Chiang <hc57@rice.edu>
2020-11-12 22:48:31 -0600

    initial commit

--- index.js
+++ index.js
@@ -5,45 +5,8 @@ const follow = require('./src/follower');
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const mongoose = require('mongoose');
 const cors=require('cors');
-const connectionString = 'mongodb+srv://LarsChiang:c1LKbgzrbPXWcvQU@cluster0.vfpzw.mongodb.net/HW6?retryWrites=true&w=majority';
 
-
-
-
-/*
-let articles = [{ id: 0, author: 'Mack', body: 'Post 1' },
-    { id: 1, author: 'Jack', body: 'Post 2' },
-    { id: 2, author: 'Zack', body: 'Post 3' }];
-
-/*
-const hello = (req, res) => res.send({ hello: 'world' });
-
-const getUser = (req, res) => {
-    (async () => {
-        const connector =   mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
-       
-        await (connector.then(()=> {
-        	return new User({
-				username: req.params.uname,
-				created: Date.now()
-			}).save();
-        }));
-        res.send({name: req.params.uname});
-    })();
-
-};
-const getArticles = (req, res) => res.send(articles);
-
-const getArticle = (req, res) => res.send(articles[req.params.id]);
-
-const addArticle = (req, res) => {
-    let post = req.body;
-    let article = {id: articles.length, author: post.author, body: post.body}
-    articles.push(article);
-    res.send(articles);
-}*/
 const app = express();
 app.use(cors({
     origin:　true,
